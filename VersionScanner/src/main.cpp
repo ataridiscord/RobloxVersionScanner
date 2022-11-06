dd062607fe8665764877d0b973dda37f5f100693
atari <84478811+atariXD@users.noreply.github.com>
2022-11-06 08:11:02 +0000

    Removed unnecessary code.

--- VersionScanner/src/main.cpp
+++ VersionScanner/src/main.cpp
@@ -1,103 +1,39 @@
 #include <iostream>
-#include <curl/curl.h>
-#include <Windows.h>
-#include <string>
-#include <format>
+#include <cpr/cpr.h>
 #include <filesystem>
-#include <shlobj.h>
+#include <shlobj_core.h>
+#include <KnownFolders.h>
 
-__forceinline std::size_t writeFunction(void* ptr, unsigned long long size, unsigned long long nmemb, std::string* data)
+#pragma warning(push)
+#pragma warning(disable:4996)
+inline std::string get_appdata_path()
 {
-    (std::string)data->append((char*)ptr, size * nmemb);
-    return size * nmemb;
+    PWSTR appdata_path{};
+    if (SHGetKnownFolderPath(FOLDERID_LocalAppData, 0, NULL, &appdata_path))
+        return EXIT_FAILURE;
+
+    char buff[MAX_PATH];
+    wcstombs(buff, appdata_path, MAX_PATH);
+    delete appdata_path;
+
+    return buff;
 }
+#pragma warning(pop)
 
 int main()
 {
-    std::string response{};
-    std::string header{};
-    auto response_code{0ul};
+    SetConsoleTitleA("Version Scanner");
 
-    PWSTR data = NULL;
-    char dest[MAX_PATH];
+    const auto current_version = cpr::Get(cpr::Url{"http://setup.roblox.com/version"}).text;
+    std::printf("Looking for version %s...\n", current_version);
 
-    if (SHGetKnownFolderPath(FOLDERID_LocalAppData, KF_FLAG_DEFAULT, NULL, &data) == S_OK) {
-        wcstombs(dest, data, MAX_PATH);
-    }
-    else {
-        std::printf("Failed to fetch AppData path.\n");
-        std::cin.get();
-        return EXIT_FAILURE;
-    }
+    const auto path = (get_appdata_path() + "\\Roblox\\Versions\\" + current_version);
 
-    std::string appdata = dest;
+    if (!std::filesystem::exists(path))
+        std::printf("Unable to find latest Roblox version.\n");
+    else
+        std::printf("Found latest Roblox version.\n");
 
-    const auto versions_path = appdata + std::string("\\Roblox\\Versions\\");
-    const auto latest_version_path = appdata + std::string("\\Roblox\\Versions\\") + response;
-
-    SetConsoleTitleA("Version Scanner - atari#5297");
-
-    std::printf("This project is licensed under: GNU GENERAL PUBLIC LICENSE\n"
-        "https://github.com/atari-1337/VersionScanner/blob/main/LICENSE\n\n"
-        "Fetching current Roblox version...\n");
-
-    curl_global_init(CURL_GLOBAL_DEFAULT);
-    auto session = curl_easy_init();
-
-    if (!session)
-        return EXIT_FAILURE;
-
-    curl_easy_setopt(session, CURLOPT_URL, "http://setup.roblox.com/version");
-    curl_easy_setopt(session, CURLOPT_NOPROGRESS, 1L);
-    curl_easy_setopt(session, CURLOPT_MAXREDIRS, 50L);
-    curl_easy_setopt(session, CURLOPT_TCP_KEEPALIVE, 1L);
-
-    curl_easy_setopt(session, CURLOPT_WRITEFUNCTION, writeFunction);
-    curl_easy_setopt(session, CURLOPT_WRITEDATA, &response);
-    curl_easy_setopt(session, CURLOPT_HEADERDATA, &header);
-
-    auto code = curl_easy_perform(session);
-
-    curl_easy_getinfo(session, CURLINFO_RESPONSE_CODE, &response_code);
-    if (response_code != 200)
-    {
-        std::printf("Failed to fetch latest Roblox version.\n");
-        std::cin.get();
-        return EXIT_FAILURE;
-    }
-
-    curl_easy_cleanup(session);
-    curl_global_cleanup();
-
-    std::cout << "Current Roblox Version: " << response << "\n";
-
-    if (!std::filesystem::exists(versions_path))
-    {
-        std::printf("Failed to locate the Roblox path.\n");
-        std::cin.get();
-        return EXIT_FAILURE;
-    }
-
-    if (!std::filesystem::exists(latest_version_path))
-    {
-        std::printf("[INFO]: Your Roblox Client is outdated, would you like to update?\n");
-        std::printf("Y/N: ");
-        if (std::cin.get() != 'y')
-            return EXIT_SUCCESS;
-        const auto sh_path = "C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Roblox\\Roblox Player";
-        switch (std::filesystem::exists(sh_path))
-        {
-        case true:
-            system(std::format("runas.exe / savecred / user:administrator {}", sh_path).c_str());
-            std::this_thread::sleep_for(std::chrono::seconds(3));
-            break;
-
-        default:
-            std::printf("[ERROR]: Failed to locate Roblox Player.\n");
-            std::cin.get();
-            return EXIT_FAILURE;
-            break;
-        }
-        return EXIT_SUCCESS;
-    }
+    std::cin.get();
+    return EXIT_SUCCESS;
 }
